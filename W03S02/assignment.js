d5952c69580ca5d3b2f93331d75e5165ab5c0d18
aditya.siregar <aditya.siregar@ovo.com>
2022-11-25 21:26:05 +0800

    Update Async

--- W03S02/assignment.js
+++ W03S02/assignment.js
@@ -1,34 +1,50 @@
-async function rendam() {
+  async function rendam(cb) {
     await setTimeout(function() {
       console.log("Mulai rendam. Setelah ini kucek")
+      cb()
     }, 3000);
   }
-  async function kucek() {
+
+  async function kucek(cb) {
     setTimeout(function() {
       console.log("Sedang kucek. Setelah ini bilas")
+      cb()
     }, 2000);
   }
-  async function bilas() {
+
+  async function bilas(cb) {
     setTimeout(function() {
       console.log("Sedang bilas. Setelah ini jemur")
+      cb()
     }, 1000);
   }
-  async function jemur() {
+
+  async function jemur(cb) {
     setTimeout(function() {
       console.log("Sedang jemur. Setelah ini setrika")
+      cb()
     }, 5000);
   }
+
   async function setrika() {
     setTimeout(function() {
       console.log("Sedang setrika. Setelah ini selesai")
     }, 4000);
   }
- rendam()
- kucek();
- bilas();
- jemur();
- setrika();
- 
+
+function myCallback() {
+    kucek(function() {
+        bilas(function(){
+            jemur(function() {
+                setrika()
+            })
+        })
+    })
+}
+
+rendam(myCallback)
+
+//  nyuci()
  // Sedang bilas. Setelah ini jemur
  // Sedang kucek. Setelah ini bilas
  // Mulai rendam. Setelah ini kucek
